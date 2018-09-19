6b808897d3d47dcce5147507abeb55dc1a276ab1
gsh-go <sihangong@163.com>
2018-09-19 08:01:43 +0800

    添加多线程模块

---
+++ src/main/java/com/gsh/concurrent/mtpattern/ch6/promise/example/FTPUploader.java
@@ -0,0 +1,12 @@
+package com.gsh.concurrent.mtpattern.ch6.promise.example;
+
+import java.io.File;
+
+public interface FTPUploader {
+    void init(String ftpServer, String ftpUserName, String password,
+              String serverDir) throws Exception;
+
+    void upload(File file) throws Exception;
+
+    void disconnect();
+}
