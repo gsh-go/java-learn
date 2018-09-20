c6e25bd905cd4f15b416d35709947a48212d2726
gsh-go <sihangong@163.com>
2018-09-20 08:44:44 +0800

    添加静态代理

---
+++ src/main/java/com/gsh/designpatterns/proxy/statics/Wizard.java
@@ -0,0 +1,21 @@
+package com.gsh.designpatterns.proxy.statics;
+
+/**
+ *
+ * Wizard
+ *
+ */
+public class Wizard {
+
+    private final String name;
+
+    public Wizard(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
---
+++ src/main/java/com/gsh/designpatterns/proxy/statics/WizardTower.java
@@ -0,0 +1,9 @@
+package com.gsh.designpatterns.proxy.statics;
+
+/**
+ * WizardTower interface
+ */
+public interface WizardTower {
+
+  void enter(Wizard wizard);
+}
